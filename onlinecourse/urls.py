82828be2f73adba6c5148802a35fd1e331a59d7e
LeOn0231 <lleonni86@gmail.com>
2025-05-22 10:46:31 -0400

    first commit

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = 'onlinecourse'
 urlpatterns = [
     # route is a string contains a URL pattern
+    path('<int:course_id>/submit/', views.submit, name="submit"),
     # view refers to the view function
     # name the URL
     path(route='', view=views.CourseListView.as_view(), name='index'),
@@ -18,7 +19,8 @@ urlpatterns = [
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
     # <HINT> Create a route for submit view
-
+    path('<int:course_id>/submit/', views.submit, name="submit"),
     # <HINT> Create a route for show_exam_result view
-
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name="exam_result"),
+    
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
